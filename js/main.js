3c72618f6d30aaa0de0a11a42a969e443e9d2221
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2019-11-25 20:06:06 +0900

    タイマーを表示させよう

--- js/main.js
+++ js/main.js
@@ -5,4 +5,23 @@
   const start = document.getElementById('start');
   const stop = document.getElementById('stop');
   const reset = document.getElementById('reset');
+
+  let startTime;
+
+  function countup() {
+    const d = new Date(Date.now() - startTime);
+    const m = String(d.getMinutes()).padStart(2, '0');
+    const s = String(d.getSeconds()).padStart(2, '0');
+    const ms = String(d.getSeconds()).padStart(3, '0');
+    timer.textContent = `${m}:${s}.${ms}`;
+
+    setTimeout(() => {
+      countup();
+    }, 10);
+  }
+
+ start.addEventListener('click', () => {
+   startTime = Date.now();
+   countup();
+ });
 }
