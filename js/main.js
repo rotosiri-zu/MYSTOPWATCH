53690ce213fcf8e11099ebaea381b3c40a37d859
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2019-11-25 23:00:47 +0900

    停止､リセットの機能を実装しよう

--- js/main.js
+++ js/main.js
@@ -7,21 +7,30 @@
   const reset = document.getElementById('reset');
 
   let startTime;
+  let timeoutId;
 
-  function countup() {
+  function countUp() {
     const d = new Date(Date.now() - startTime);
     const m = String(d.getMinutes()).padStart(2, '0');
     const s = String(d.getSeconds()).padStart(2, '0');
-    const ms = String(d.getSeconds()).padStart(3, '0');
+    const ms = String(d.getMilliseconds()).padStart(3, '0');
     timer.textContent = `${m}:${s}.${ms}`;
 
-    setTimeout(() => {
-      countup();
+    timeoutId = setTimeout(() => {
+      countUp();
     }, 10);
   }
-
  start.addEventListener('click', () => {
    startTime = Date.now();
-   countup();
+   countUp();
  });
+
+ stop.addEventListener('click', () => {
+   clearTimeout(timeoutId);
+ });
+
+ reset.addEventListener('click', () => {
+   timer.textContent = '00:00.000';
+ });
+
 }
